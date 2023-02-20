aaa698cbc7e83c9b0396e153c97f8a4e4ea22dae
MalenaLuana <malenaluana98@gmail.com>
2023-02-20 18:49:52 -0300

    sing up validations and postUser

--- api/src/controllers/Users.controllers.js
+++ api/src/controllers/Users.controllers.js
@@ -1,67 +1,76 @@
-const {Users,Note} = require('../db')
+const { Users, Note } = require('../db')
 const bcrypt = require('bcrypt');
- 
-const getUSers = async (req,res)=>{
+
+const getUSers = async (req, res) => {
 
     try {
-        const DB_users= await Users.findAll()
-        console.log('get users')
+        const DB_users = await Users.findAll()
         res.json(DB_users)
     } catch (error) {
-        res.send('error:',error.message)
+        res.send('error:', error.message)
     }
 
 }
 
-const createUSers = async (req,res)=>{
+const createUSers = async (req, res) => {
+
+    const { name, email, password } = req.body
 
     try {
-        const {name,email,password}= req.body
-        const newUser = await Users.create({
-            name,
-            email,
-            password
+        const user = await Users.findOne({
+            where: {
+                email
+            }
+
         })
-        res.json(newUser)
- 
+        if (user) {
+            res.send('Ya existe un usuario con esa cuenta de mail')
+        } else {
+            const newUser = await Users.create({
+                name,
+                email,
+                password
+            })
+            res.json(newUser)
+        }
+
     } catch (error) {
-        res.send('error',error.message)
+        res.send('error', error.message)
     }
-   
- 
+
 }
 
-const deleteUsers = async (req,res)=>{
-    const {id} = req.body
-try {
-     await Users.destroy({
-        where:{
-            id
-        }
-     })
-     res.send('Usuario borrado con éxito')
-} catch (error) {
-    res.status(404).send(error.message)
-}
-}
-
-const getUserNotes = async (req,res)=>{
- const {id}= req.params
-try {
-    const userNotes = await Note.findAll({
-        where:{
-            userID:id
-        }
-     })
-     res.json(userNotes)
-} catch (error) {
-    res.status(404).send(error.message)
-}
+const deleteUsers = async (req, res) => {
+    const { id } = req.body
+    try {
+        await Users.destroy({
+            where: {
+                id
+            }
+        })
+        res.send('Usuario borrado con éxito')
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
 }
 
-module.exports= {
-  getUSers,
-  createUSers,
-  deleteUsers,
-  getUserNotes
+const getUserNotes = async (req, res) => {
+    const { id } = req.params
+    try {
+        const userNotes = await Note.findAll({
+            where: {
+                userID: id
+            }
+        })
+        res.json(userNotes)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+}
+
+module.exports = {
+    getUSers,
+    createUSers,
+    deleteUsers,
+    getUserNotes
 }
